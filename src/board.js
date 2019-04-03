ea7c5f26feab5cea79835b0b77b65fc20c7d6096
Anna Pawl <annampawl@gmail.com>
2019-04-03 13:49:58 -0700

    Taking turns works

--- src/board.js
+++ src/board.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
-class Board extends Component {
+class Board extends React.Component {
   constructor(props) {
     super(props);
-    // this = {}
     this.state = {
-      // this = { state:{} }
-      // this = { state:{squares:[]} }
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -23,7 +24,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
